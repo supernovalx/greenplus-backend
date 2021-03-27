2eb59810a7c33eab710e3403bb616ea30ca707ed
supernovalx <hoanganhgo9@gmail.com>
2021-03-27 18:53:34 +0700

    Fix routes not working

--- src/main.ts
+++ src/main.ts
@@ -2,42 +2,29 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import {
-  ExpressAdapter,
-  NestExpressApplication,
-} from '@nestjs/platform-express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import {
   DocumentBuilder,
   SwaggerCustomOptions,
   SwaggerDocumentOptions,
   SwaggerModule,
 } from '@nestjs/swagger';
+import * as fs from 'fs';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { TrimPipe } from './common/decorator/trim.pipe';
 import { GlobalConfigKey } from './modules/global-config/config-keys';
 import { GlobalConfigRepository } from './modules/global-config/global-config.repository';
-import * as fs from 'fs';
-import * as http from 'http';
-import * as https from 'https';
-// import express from 'express';
-const express = require('express');
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    new ExpressAdapter(server),
-  );
-
-  // Get port
-  const configService = app.get(ConfigService);
-  const PORT = Number(configService.get<number>('PORT'));
-
-  http.createServer(server).listen(PORT || 3000);
-  const httpsOptions: HttpsOptions | null = readHttpsCertificate();
-  if (httpsOptions !== null) {
-    https.createServer(httpsOptions, server).listen(443);
+  const httpOptions: HttpsOptions | null = readHttpsCertificate();
+  let app;
+  if (httpOptions !== null) {
+    app = await NestFactory.create<NestExpressApplication>(AppModule, {
+      httpsOptions: httpOptions,
+    });
+  } else {
+    app = await NestFactory.create<NestExpressApplication>(AppModule);
   }
 
   // Swagger
@@ -57,13 +44,15 @@ async function bootstrap() {
   };
   SwaggerModule.setup('api-docs', app, document, customOptions);
 
-  await app.init();
-
   // Serve static files
   app.useStaticAssets(join(__dirname, '..', 'upload'));
 
   // class-validator, trim
   app.useGlobalPipes(new ValidationPipe({ transform: true }), new TrimPipe());
+
+  // Get port
+  const configService = app.get(ConfigService);
+  const PORT = Number(configService.get<number>('PORT'));
 
   // Seeding
   const globalConfigRepository = app.get(GlobalConfigRepository);
@@ -71,6 +60,8 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({ origin: '*' });
+
+  await app.listen(PORT || 3000);
 }
 
 async function seedGlobalClosureDates(
